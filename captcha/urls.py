745d9c244ad9cfe9ba59549e13571f1d492ead51
Roman Sova <roman.sova@gmail.com>
2014-12-02 20:07:40 +0100

    django 1.7 urls

--- captcha/urls.py
+++ captcha/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls.defaults import *
+
+except ImportError:
+    from django.conf.urls import *
 
 urlpatterns = patterns('captcha.views',
     url(r'image/(?P<key>\w+)/$','captcha_image',name='captcha-image'),
-    url(r'audio/(?P<key>\w+)/$','captcha_audio',name='captcha-audio'),    
+    url(r'audio/(?P<key>\w+)/$','captcha_audio',name='captcha-audio'),
 )
