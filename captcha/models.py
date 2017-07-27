16588c4ccf1b4a49307e29c6d90deff20bf10640
Roman Sova <roman.sova@gmail.com>
2017-07-27 08:43:10 +0200

    hashkey exists error fix

--- captcha/models.py
+++ captcha/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from captcha.conf import settings as captcha_settings
 import datetime
+
 try:
     import hashlib # sha for Python 2.5+
 except ImportError:
@@ -12,23 +13,29 @@ class CaptchaStore(models.Model):
     response = models.CharField(blank=False, max_length=32)
     hashkey = models.CharField(blank=False, max_length=40,unique=True)
     expiration = models.DateTimeField(blank=False)
-    
+
     def save(self,*args,**kwargs):
         self.response = self.response.lower()
         if not self.expiration:
             self.expiration = datetime.datetime.now() + datetime.timedelta(minutes= int(captcha_settings.CAPTCHA_TIMEOUT))
+
         if not self.hashkey:
             if hashlib:
                 self.hashkey = hashlib.new('sha', str(self.challenge) + str(self.response)).hexdigest()
             else:
                 self.hashkey = sha.new(str(self.challenge) + str(self.response)).hexdigest()
+
+        if self.pk:
+            CaptchaStore.objects.filter(pk__not=self.pk, hashkey=self.hashkey).delete()
+
+        else:
+            CaptchaStore.objects.filter(hashkey=self.hashkey).delete()
+
         super(CaptchaStore,self).save(*args,**kwargs)
 
     def __unicode__(self):
         return self.challenge
 
-    
     def remove_expired(cls):
         cls.objects.filter(expiration__lte=datetime.datetime.now()).delete()
     remove_expired = classmethod(remove_expired)
-    
